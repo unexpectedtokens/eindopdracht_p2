f421623bfdf99c2554c5b12e6e5cd0c22969cc8f
daniel de jong <daniel@MacBook-Pro.local>
2019-02-04 10:42:33 +0100

    server fixed

--- public/script/script.js
+++ public/script/script.js
@@ -1,9 +1,32 @@
 $(document).ready(function() {
   $(".dropdown").click(function() {
     $(".dropdown-content").fadeToggle(300);
-    //   $(".dropdown-content").fadeIn(300);
-    // } else if ($(".dropdown-content").show) {
-    //   $(".dropdown-content").fadeOut(300);
-    // }
+  });
+  $(".hamburger").click(function() {
+    $(".hamburger-dropdown").fadeToggle(300);
+  });
+  $(".contact-nav").click(function() {
+    $(".main-second").hide(100);
+    $(".main").fadeOut(100);
+    $(".contact-section").fadeIn(300);
+    $(".product-info").hide(100);
+  });
+  $(".home").click(function() {
+    $(".main").fadeIn(200);
+    $(".contact-section").hide(100);
+    $(".product-info").hide(100);
+    $(".main-second").hide(100);
+  });
+  $(".aaa").click(function() {
+    $(".main-second").fadeIn(200);
+    $(".main").hide(100);
+    $(".contact-section").hide(100);
+    $(".product-info").hide(100);
+  });
+  $(".product-container").click(() => {
+    $(".main-second").fadeOut(200);
+    $(".main").hide(100);
+    $(".contact-section").hide(100);
+    $(".product-info").show(100);
   });
 });
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const bp = require("body-parser");
+
+// const nodemailer = require("nodemailer");
+let urlEncodedParser = bp.urlencoded({ extended: false });
 const app = express();
 var port = process.env.port || 4200;
 
@@ -8,6 +12,37 @@ app.get("/", (req, res) => {
   res.render("index.html");
 });
 
+app.get("/contacted.html", (req, res) => {
+  res.render("contacted.html");
+});
+
+app.post("/contacted.html", urlEncodedParser, (req, res) => {
+  console.log(req.body);
+  // res.render("/contacted.html");
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+// let transporter = nodemailer.createTransport({
+//   service: "g-mail",
+//   auth: {
+//     user: "dejong543@gmail.com",
+//   }
+// });
+
+// let mailOptions = {
+//   from: "dejong543@info.com",
+//   to: "mputto@icloud.com",
+//   subject: "Sending Email using Node.js",
+//   text: "That was easy!"
+// };
+
+// transporter.sendMail(mailOptions, function(error, info) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Email sent: " + info.response);
+//   }
+// });
